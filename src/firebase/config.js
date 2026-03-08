849918db8f2650714b80899d8a626544c0e76eb4
Jesús Rodríguez Sánchez <jesusrodriguezsanchez@Jesuss-MacBook-Air.local>
2026-03-08 18:07:08 +0100

    fix: point storage to production bucket

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -22,7 +22,7 @@ const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyABSDAGQXQizE2uPMXnDudTfvQK77rLqpk",
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "gymtracker-a01c8.firebaseapp.com",
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || PROD_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "gymtracker-a01c8.appspot.com",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "gymtracker-a01c8.firebasestorage.app",
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "132751081821",
   appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:132751081821:web:d95d79b8e207c9c36368b4",
 };
